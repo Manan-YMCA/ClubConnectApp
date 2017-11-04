b70ac6628386d7e91bcd70286b59d69b6d9f6a3c
namansachdeva <namansachdevaa@gmail.com>
2017-11-04 15:41:15 +0530

    Admin Dashboard Initiated

--- app/src/main/java/com/manan/dev/clubconnect/AdminDashboardActivity.java
+++ app/src/main/java/com/manan/dev/clubconnect/AdminDashboardActivity.java
@@ -1,13 +1,23 @@
 package com.manan.dev.clubconnect;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AdminDashboardActivity extends AppCompatActivity {
+    private FloatingActionButton addNewEventFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
+        addNewEventFab = (FloatingActionButton) findViewById(R.id.add_new_event_fab);
+        addNewEventFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
