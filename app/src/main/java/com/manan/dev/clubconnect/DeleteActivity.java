f156e631b20a6357ca33f634b68b7a65242e4e75
YATIN DHINGRA <yatind01@gmail.com>
2017-11-07 15:36:35 +0530

    Menu folder

--- app/src/main/java/com/manan/dev/clubconnect/DeleteActivity.java
+++ app/src/main/java/com/manan/dev/clubconnect/DeleteActivity.java
@@ -2,6 +2,7 @@ package com.manan.dev.clubconnect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class DeleteActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class DeleteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delete);
+        LinearLayout
     }
 }
