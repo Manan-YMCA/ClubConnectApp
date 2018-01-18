8cef38689c7a75f6d38c4693e3e3b8c3f656c8b3
YATIN DHINGRA <yatind01@gmail.com>
2018-01-18 16:22:02 +0530

    minor bug fixes

--- app/src/main/java/com/manan/dev/clubconnect/user/UserClubEventListActivity.java
+++ app/src/main/java/com/manan/dev/clubconnect/user/UserClubEventListActivity.java
@@ -8,13 +8,16 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.manan.dev.clubconnect.Adapters.UserSingleEventListAdapter;
 import com.manan.dev.clubconnect.Models.Event;
+import com.manan.dev.clubconnect.Models.UserData;
 import com.manan.dev.clubconnect.R;
 
 import java.util.ArrayList;
@@ -24,11 +27,16 @@ import static com.manan.dev.clubconnect.Adapters.UserSingleEventListAdapter.CLUB
 public class UserClubEventListActivity extends AppCompatActivity {
 
     RecyclerView userSingleEventListRV;
-    ArrayList<Event> userSingleEventListArrayList;
+    ArrayList<Event> userSingleEventListArrayList , allEvents;
     UserSingleEventListAdapter userSingleEventListAdapter;
     private DatabaseReference mDatabaseReference;
     String clubName;
     private ChildEventListener mChildEventListener;
+    private ChildEventListener mprivateChildEventListener;
+    private String userId;
+    private UserData user;
+    private DatabaseReference mPrivateDatabaseEvents;
+    private ArrayList<String> myClubs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +64,11 @@ public class UserClubEventListActivity extends AppCompatActivity {
         userSingleEventListRV.setAdapter(userSingleEventListAdapter);
 
         mDatabaseReference = FirebaseDatabase.getInstance().getReference().child("events").child(clubName);
+        userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        user = new UserData();
+        allEvents = new ArrayList<>();
+        myClubs = new ArrayList<>();
+        mPrivateDatabaseEvents = FirebaseDatabase.getInstance().getReference().child("users");
 
 
         ActionBar actionBar = getSupportActionBar();
@@ -86,12 +99,16 @@ public class UserClubEventListActivity extends AppCompatActivity {
         super.onPause();
         detatchDatabaseListener();
         userSingleEventListArrayList.clear();
+        allEvents.clear();
     }
 
     private void detatchDatabaseListener() {
         if (mChildEventListener != null) {
             mDatabaseReference.removeEventListener(mChildEventListener);
             mChildEventListener = null;
+        }
+        if(mprivateChildEventListener != null){
+            mPrivateDatabaseEvents.removeEventListener(mprivateChildEventListener);
         }
     }
 
@@ -105,36 +122,26 @@ public class UserClubEventListActivity extends AppCompatActivity {
                         Event obj = dataSnapshot.getValue(Event.class);
                         obj.setEventId(dataSnapshot.getKey());
                         obj.setClubName(clubName);
-                        userSingleEventListArrayList.add(obj);
+                        allEvents.add(obj);
+                        updateList();
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
-                    userSingleEventListAdapter = new UserSingleEventListAdapter(userSingleEventListArrayList, UserClubEventListActivity.this);
-                    userSingleEventListRV.setAdapter(userSingleEventListAdapter);
 
                 }
 
                 @Override
                 public void onChildChanged(DataSnapshot dataSnapshot, String s) {
                     try {
-                        Log.d("onAdded", dataSnapshot.toString());
-
+                        //Log.d("onAdded", dataSnapshot.toString());
                         Event obj = dataSnapshot.getValue(Event.class);
                         obj.setEventId(dataSnapshot.getKey());
                         obj.setClubName(clubName);
-
-                        for (int i = 0; i < userSingleEventListArrayList.size(); i++)
-                            if (userSingleEventListArrayList.get(i).getEventId().equals(dataSnapshot.getKey())) {
-                                userSingleEventListArrayList.set(i, obj);
-                                break;
-                            }
-
+                        allEvents.add(obj);
+                        updateList();
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
-
-                    userSingleEventListAdapter = new UserSingleEventListAdapter(userSingleEventListArrayList, UserClubEventListActivity.this);
-                    userSingleEventListRV.setAdapter(userSingleEventListAdapter);
                 }
 
                 @Override
@@ -158,6 +165,66 @@ public class UserClubEventListActivity extends AppCompatActivity {
             };
             mDatabaseReference.addChildEventListener(mChildEventListener);
         }
+        if(mprivateChildEventListener == null){
+            mprivateChildEventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    if (dataSnapshot.getKey().equals(userId)) {
+                        user = dataSnapshot.getValue(UserData.class);
+                        if (user != null && user.getEmailId() != null) {
+                            Log.d("user id", user.getEmailId());
+                        } else
+                            Log.d("user id", "null");
+                        updateList();
+                    }
+                }
+
+                @Override
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                    if (dataSnapshot.getKey().equals(userId)) {
+                        user = dataSnapshot.getValue(UserData.class);
+                        if (user != null && user.getEmailId() != null) {
+                            Log.d("user id", user.getEmailId());
+                        } else
+                            Log.d("user id", "null");
+                        updateList();
+                    }
+                }
+
+                @Override
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            };
+            mPrivateDatabaseEvents.addChildEventListener(mprivateChildEventListener);
+        }
+    }
+
+    private void updateList() {
+        userSingleEventListArrayList.clear();
+        if(user.getMyClubs() != null)
+            myClubs.addAll(user.getMyClubs());
+
+        for(Event ev : allEvents){
+            if(ev.getPrivate()){
+                if(myClubs.contains(ev.getClubName())){
+                    userSingleEventListArrayList.add(ev);
+                }
+            } else if(!ev.getPrivate()){
+                userSingleEventListArrayList.add(ev);
+            }
+        }
+        userSingleEventListAdapter.notifyDataSetChanged();
     }
 
 }
